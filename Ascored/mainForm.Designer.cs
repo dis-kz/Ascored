d53c819f329716a426af4206aae984629d7368c6
Иван С. Дежин <ivands@tcoil.ru>
2019-02-04 14:29:51 +0300

    - проект создан

--- Ascored/mainForm.Designer.cs
+++ Ascored/mainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ascored
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(661, 417);
+            this.Name = "mainForm";
+            this.Text = "Welcome to Ascored";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
